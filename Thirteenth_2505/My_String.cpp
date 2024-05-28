440f836e70180dc0da66eaa5408358434113d014
KorotnitskiVladimir <136509147+KorotnitskiVladimir@users.noreply.github.com>
2024-05-28 22:49:34 +0300

    Add files via upload
    basis of My_String class completed

---
+++ Thirteenth_2505/My_String.cpp
@@ -0,0 +1,18 @@
+#include "str.h"
+
+int main(int argc, char* argv[])
+{
+    //String s1;
+    //s1.setStr();
+    //cout << s1.getStr() << endl;
+    //String s2(255);
+    //s2.setStr();
+    //cout << s2.getStr() << endl;
+    //String s3("What a wonderful day!");
+    //cout << s3.getStr() << endl;
+    //char p[] = "Good Night";
+    //String s4(p);
+    //cout << s4.getStr() << endl;
+    
+    return 0;
+}
---
+++ Thirteenth_2505/str.h
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+using namespace std;
+
+class String
+{
+private:
+    char* str;
+public:
+    String()
+    {
+        str = new char[81];
+    }
+    String(const char* s)
+    {
+        str = new char[strlen(s)+1];
+        strcpy_s(str, strlen(s)+1, s);
+    }
+    String(int n)
+    {
+        str = new char[n+1];
+    }
+    ~String()
+    {
+        delete [] str;
+    }
+    void setStr();
+    char* getStr();
+};
+
+void String::setStr()
+{
+    cout << "enter string -> " << endl;
+    char buff[255];
+    gets_s(buff);
+    if (strlen(buff)>0)
+    {
+        str = new char[strlen(buff)+1];
+        strcpy_s(str, strlen(buff)+1, buff);
+    }
+    else
+        cout << "not able to create string" << endl;
+}
+char* String::getStr()
+{
+    return str;
+}
+
